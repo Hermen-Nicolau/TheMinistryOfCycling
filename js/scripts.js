d7830b1b3ccc8b9cc336ae0f03145908f6b8547a
Hermen-Nicolau <hermen.nicolau@gmail.com>
2020-12-04 21:03:12 -0500

    finished the reviews main page

---
+++ js/scripts.js
@@ -0,0 +1,149 @@
+var mymap = L.map('generalMap').setView([36.099861, -80.244217], 11);
+
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZml0aXBhbGRpIiwiYSI6ImNraTlzZXU4dDA1NDAzMHRlcHB6Y2E3ZDYifQ.pdGQjChKJuW8YIv3s4vAkg'
+}).addTo(mymap);
+
+
+//Adding a marker for Hobby
+var marker1 = L.marker([36.0327, -80.3058]).addTo(mymap);
+marker1.bindPopup("<b>Hobby Park</b><br>").openPopup();
+
+//Adding a marker for Tanglewood
+var marker2 = L.marker([36.0054, -80.4082]).addTo(mymap);
+marker2.bindPopup("<b>Tanglewood Park</b><br>").openPopup();
+
+//Adding a marker for Kernersville MTB Park
+var marker3 = L.marker([36.12354, -80.04186]).addTo(mymap);
+marker3.bindPopup("<b>Kernersville MTB </b><br>").openPopup();
+
+//Adding a marker for Salem Lake
+var marker4 = L.marker([36.0940, -80.1932]).addTo(mymap);
+marker4.bindPopup("<b>Salem Lake</b><br>").openPopup();
+
+//Weather
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+
+
+//Rating code
+var __slice = [].slice;
+
+(function($, window) {
+    var Starrr;
+
+    Starrr = (function() {
+        Starrr.prototype.defaults = {
+            rating: void 0,
+            numStars: 5,
+            change: function(e, value) {}
+        };
+
+        function Starrr($el, options) {
+            var i, _, _ref,
+                _this = this;
+
+            this.options = $.extend({}, this.defaults, options);
+            this.$el = $el;
+            _ref = this.defaults;
+            for (i in _ref) {
+                _ = _ref[i];
+                if (this.$el.data(i) != null) {
+                    this.options[i] = this.$el.data(i);
+                }
+            }
+            this.createStars();
+            this.syncRating();
+            this.$el.on('mouseover.starrr', 'i', function(e) {
+                return _this.syncRating(_this.$el.find('i').index(e.currentTarget) + 1);
+            });
+            this.$el.on('mouseout.starrr', function() {
+                return _this.syncRating();
+            });
+            this.$el.on('click.starrr', 'i', function(e) {
+                return _this.setRating(_this.$el.find('i').index(e.currentTarget) + 1);
+            });
+            this.$el.on('starrr:change', this.options.change);
+        }
+
+        Starrr.prototype.createStars = function() {
+            var _i, _ref, _results;
+
+            _results = [];
+            for (_i = 1, _ref = this.options.numStars; 1 <= _ref ? _i <= _ref : _i >= _ref; 1 <= _ref ? _i++ : _i--) {
+                _results.push(this.$el.append("<i class='fa fa-star-o'></i>"));
+            }
+            return _results;
+        };
+
+        Starrr.prototype.setRating = function(rating) {
+            if (this.options.rating === rating) {
+                rating = void 0;
+            }
+            this.options.rating = rating;
+            this.syncRating();
+            return this.$el.trigger('starrr:change', rating);
+        };
+
+        Starrr.prototype.syncRating = function(rating) {
+            var i, _i, _j, _ref;
+
+            rating || (rating = this.options.rating);
+            if (rating) {
+                for (i = _i = 0, _ref = rating - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+                    this.$el.find('i').eq(i).removeClass('fa-star-o').addClass('fa-star');
+                }
+            }
+            if (rating && rating < 5) {
+                for (i = _j = rating; rating <= 4 ? _j <= 4 : _j >= 4; i = rating <= 4 ? ++_j : --_j) {
+                    this.$el.find('i').eq(i).removeClass('fa-star').addClass('fa-star-o');
+                }
+            }
+            if (!rating) {
+                return this.$el.find('i').removeClass('fa-star').addClass('fa-star-o');
+            }
+        };
+
+        return Starrr;
+
+    })();
+    return $.fn.extend({
+        starrr: function() {
+            var args, option;
+
+            option = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+            return this.each(function() {
+                var data;
+
+                data = $(this).data('star-rating');
+                if (!data) {
+                    $(this).data('star-rating', (data = new Starrr($(this), option)));
+                }
+                if (typeof option === 'string') {
+                    return data[option].apply(data, args);
+                }
+            });
+        }
+    });
+})(window.jQuery, window);
+
+$(function() {
+    return $(".starrr").starrr();
+});
+
+$( document ).ready(function() {
+      
+  $('#stars').on('starrr:change', function(e, value){
+    $('#count').html(value);
+  });
+  
+  $('#stars-existing').on('starrr:change', function(e, value){
+    $('#count-existing').html(value);
+  });
+});
